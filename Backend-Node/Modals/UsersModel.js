5b8d57d238720e38d471edda7730bb1c2ec86e6f
Ashish-Abraham <temp200295@cet.ac.in>
2023-03-24 11:33:16 +0530

    added faculty model

--- Backend-Node/Modals/UsersModel.js
+++ Backend-Node/Modals/UsersModel.js
@@ -2,41 +2,41 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const facultySchema = new mongoose.Schema({
-    employeeNo:{
-        type:String,
-        required:true
+    employeeNo: {
+        type: String,
+        required: true
     },
-    role:{
-        type:Number,
-        required:true
+    role: {
+        type: Number,
+        required: true
     },
-    name:{
-        type:String,
-        required:true,
+    name: {
+        type: String,
+        required: true,
     },
-    department:{
-        type:String,
-        required:true
+    department: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true       
+    email: {
+        type: String,
+        required: true
     },
-    phoneNumber:{
-        type:String,
-        required:true
+    phoneNumber: {
+        type: String,
+        required: true
     },
-    image:{
-        type:String,
+    image: {
+        type: String,
         default:
     },
-    dateOfJoining:{
-        type:Date,
-        required:true       
+    dateOfJoining: {
+        type: Date,
+        required: true
     },
 });
 
 
-const User =  mongoose.model("user", UserSchema);
+const Faculty = mongoose.model("faculty", facultySchema);
 
-module.exports = {User};
+module.exports = { Faculty };
